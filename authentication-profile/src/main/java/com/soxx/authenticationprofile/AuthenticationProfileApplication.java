4d6f9d2d23e773e0bd0c7b531d20b307d046d8bb
jacky <so.jacky311@gmail.com>
2023-02-01 16:46:24 -0500

    initializate project

---
+++ authentication-profile/src/main/java/com/soxx/authenticationprofile/AuthenticationProfileApplication.java
@@ -0,0 +1,13 @@
+package com.soxx.authenticationprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationProfileApplication.class, args);
+	}
+
+}
---
+++ authentication-profile/src/test/java/com/soxx/authenticationprofile/AuthenticationProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soxx.authenticationprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
